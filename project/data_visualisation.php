eec44f9cba181ba0033f9aaca76e8ba20d56d0a5
Teo Li Xien <1902691@sit.singaporetech.edu.sg>
2020-10-20 01:47:40 +0800

    test commit #1
    Data visualization page added

---
+++ project/data_visualisation.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <title>Creole | (Contact Us)</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Creole | Bringing People Together Through Fashion">
+        <meta name="keywords" content="Fasion, Clothes, Dress, Tops, Bottoms">
+        <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="js/bootstrap.min.js?v3.4.1"></script> 
+        <script src="js/contactUs.js"></script>
+        <link href="css/webcss.css" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <!--  navigation  --> 
+        <?php
+        require_once('../protected/config.php');
+        include 'nav.inc.php';
+        ?>
+        <!--  navigation end  --> 
+        <!-- banner section -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="jumbotron banner">
+                    <h1 class="text-center banner-text"> DATA VISUALISATION </h1>
+                </div>
+            </div>
+        </div>
+        <!-- banner section end -->
+
